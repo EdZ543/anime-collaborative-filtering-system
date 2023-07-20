6ef1d8c4aa743c5163f559028f3ec2ad6d4a287f
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-07-20 16:52:43 -0400

    Add selections
    Former-commit-id: cd3341c2e571b90ecfb34ffea9ab056c1e7f3e45

--- app.py
+++ app.py
@@ -1,29 +1,48 @@
+"""The main application file for the Gradio app."""
+
 import os
 import gradio as gr
 import pandas as pd
-import numpy as np
 import requests
+import torch
+import torch.nn as nn
 from urllib import parse
 from dotenv import load_dotenv
 
 load_dotenv()
 
+MAL_CLIENT_ID = os.getenv("MAL_CLIENT_ID")
+
 anime_indexes = pd.read_csv("./data/anime_indexes.csv")
 animes = anime_indexes["Anime"].values.tolist()
 
-MAL_CLIENT_ID = os.getenv("MAL_CLIENT_ID")
+anime_embeddings = pd.read_csv("./data/anime_embeddings.csv", header=None)
+anime_embeddings = torch.tensor(anime_embeddings.values)
 
 
 def fetch_anime_image(anime):
     query_url = f"https://api.myanimelist.net/v2/anime?q={parse.quote(anime)}&limit=1"
     headers = {"X-MAL-CLIENT-ID": MAL_CLIENT_ID}
     query_response = requests.get(query_url, headers=headers)
+
     image_url = query_response.json()["data"][0]["node"]["main_picture"]["large"]
     return image_url
 
 
 def recommend(anime):
-    return None
+    anime_index = anime_indexes[anime_indexes["Anime"] == anime].index[0]
+    anime_embedding = anime_embeddings[anime_index][None]
+
+    embedding_distances = nn.CosineSimilarity(dim=1)(anime_embeddings, anime_embedding)
+    recommendation_indexes = embedding_distances.argsort(descending=True)[1:6].tolist()
+
+    recommendations = []
+    for recommendation_index in recommendation_indexes:
+        recommendation_anime = anime_indexes.iloc[recommendation_index]["Anime"]
+        recommendation_url = fetch_anime_image(recommendation_anime)
+        recommendations.append((recommendation_url, recommendation_anime))
+
+    return recommendations
 
 
 css = """
@@ -46,7 +65,7 @@ with gr.Blocks(css=css) as space:
             dropdown = gr.Dropdown(container=False, choices=animes)
             selection_image = gr.Image(show_label=False, width=225, visible=False)
 
-    gallery = gr.Gallery(label="Recommendations")
+    gallery = gr.Gallery(label="Recommendations", object_fit="scale-down")
 
     def submit(anime):
         if anime is None:
@@ -56,9 +75,11 @@ with gr.Blocks(css=css) as space:
             }
 
         selection_image_url = fetch_anime_image(anime)
+        recommendations = recommend(anime)
 
         return {
             selection_image: gr.update(visible=True, value=selection_image_url),
+            gallery: gr.update(value=recommendations),
         }
 
     dropdown.change(fn=submit, inputs=dropdown, outputs=[selection_image, gallery])
