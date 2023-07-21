69d76ded66e21c8392814a4bd00d064b0297cf37
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-07-21 10:46:21 -0400

    Optimize recommendation calculation

--- app.py
+++ app.py
@@ -1,59 +1,52 @@
 """The main application file for the Gradio app."""
 
-import os
 import gradio as gr
 import pandas as pd
-import requests
 import torch
-import torch.nn as nn
 
-MAL_CLIENT_ID = os.getenv("MAL_CLIENT_ID")
+animes_df = pd.read_csv("./data/animes.csv")
+anime_embeddings_df = pd.read_csv("./data/anime_embeddings.csv", header=None)
 
-anime_indexes = pd.read_csv("./data/anime_indexes.csv")
-animes = anime_indexes["Anime"].values.tolist()
-
-anime_embeddings = pd.read_csv("./data/anime_embeddings.csv", header=None)
-anime_embeddings = torch.tensor(anime_embeddings.values)
+title_list = animes_df["Title"].tolist()
+embeddings = torch.tensor(anime_embeddings_df.values)
 
 
-def fetch_anime_image_url(anime_id):
-    url = f"https://api.myanimelist.net/v2/anime/{anime_id}?fields=main_picture"
-    headers = {"X-MAL-CLIENT-ID": MAL_CLIENT_ID}
-    response = requests.get(url, headers=headers)
-    image_url = response["main_picture"]["large"]
-    return image_url
+def recommend(index):
+    embedding = embeddings[index]
 
+    embedding_distances = torch.nn.CosineSimilarity(dim=1)(embeddings, embedding)
+    recommendation_indexes = embedding_distances.argsort(descending=True)[1:4]
 
-def recommend(anime):
-    anime_index = anime_indexes[anime_indexes["Anime"] == anime].index[0]
-    anime_embedding = anime_embeddings[anime_index][None]
-
-    embedding_distances = nn.CosineSimilarity(dim=1)(anime_embeddings, anime_embedding)
-    recommendation_indexes = embedding_distances.argsort(descending=True)[1:7].tolist()
-    recommendations = [
-        (
-            "https://cdn.myanimelist.net/images/anime/1600/134703.jpg",
-            anime_indexes.iloc[index]["Anime"],
-        )
-        for index in recommendation_indexes
-    ]
+    recommendations = []
+    for rank, recommendation_index in enumerate(recommendation_indexes):
+        recommendation = animes_df.iloc[int(recommendation_index)]
+        value = recommendation["Image URL"]
+        label = f'{rank + 1}. {recommendation["Title"]}'
+        recommendations.append((value, label))
 
     return recommendations
 
 
-with gr.Blocks() as space:
-    gr.Markdown(
+css = """
+.gradio-container {align-items: center}
+#container {max-width: 800px}
+"""
+
+
+with gr.Blocks(css=css) as space:
+    with gr.Column(elem_id="container"):
+        gr.Markdown(
+            """
+        # Anime Collaborative Filtering System
+        This is a Pytorch recommendation model that uses neural collaborative filtering.
+        Enter an anime, and it will suggest similar shows!
         """
-    # Anime Collaborative Filtering System
-    This is a Pytorch recommendation model that uses neural collaborative filtering.
-    Enter an anime, and it will suggest similar shows!
-    """
-    )
+        )
 
-    dropdown = gr.Dropdown(label="Enter an anime", choices=animes)
+        dropdown = gr.Dropdown(label="Enter an anime", choices=title_list, type="index")
 
-    gallery = gr.Gallery(label="Recommendations", rows=2, columns=3)
+        gallery = gr.Gallery(label="Recommendations", rows=1, columns=3)
 
-    dropdown.change(fn=recommend, inputs=dropdown, outputs=gallery)
+        dropdown.change(fn=recommend, inputs=dropdown, outputs=gallery)
 
 space.launch()
