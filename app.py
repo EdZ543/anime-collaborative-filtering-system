2c794be7aca9ce6689c3b5e4ed45870bd564dcc6
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-07-20 20:31:44 -0400

    Alter gallery shape

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import pandas as pd
 import requests
 import torch
 import torch.nn as nn
-from urllib import parse
 
 MAL_CLIENT_ID = os.getenv("MAL_CLIENT_ID")
 
@@ -17,16 +16,12 @@ anime_embeddings = pd.read_csv("./data/anime_embeddings.csv", header=None)
 anime_embeddings = torch.tensor(anime_embeddings.values)
 
 
-def fetch_anime_image(anime):
-    query_url = f"https://api.myanimelist.net/v2/anime?q={parse.quote(anime)}&limit=1"
+def fetch_anime_image_url(anime_id):
+    url = f"https://api.myanimelist.net/v2/anime/{anime_id}?fields=main_picture"
     headers = {"X-MAL-CLIENT-ID": MAL_CLIENT_ID}
-    query_response = requests.get(query_url, headers=headers)
-
-    try:
-        image_url = query_response.json()["data"][0]["node"]["main_picture"]["large"]
-        return image_url
-    except:
-        return None
+    response = requests.get(url, headers=headers)
+    image_url = response["main_picture"]["large"]
+    return image_url
 
 
 def recommend(anime):
@@ -34,58 +29,31 @@ def recommend(anime):
     anime_embedding = anime_embeddings[anime_index][None]
 
     embedding_distances = nn.CosineSimilarity(dim=1)(anime_embeddings, anime_embedding)
-    recommendation_indexes = embedding_distances.argsort(descending=True)[1:].tolist()
-
-    recommendations = []
-    for recommendation_index in recommendation_indexes:
-        recommendation_anime = anime_indexes.iloc[recommendation_index]["Anime"]
-        recommendation_image = fetch_anime_image(recommendation_anime)
-        if recommendation_image is not None:
-            recommendations.append((recommendation_image, recommendation_anime))
-
-        if len(recommendations) == 5:
-            break
+    recommendation_indexes = embedding_distances.argsort(descending=True)[1:7].tolist()
+    recommendations = [
+        (
+            "https://cdn.myanimelist.net/images/anime/1600/134703.jpg",
+            anime_indexes.iloc[index]["Anime"],
+        )
+        for index in recommendation_indexes
+    ]
 
     return recommendations
 
 
-css = """
-#selection_column {align-items: center}
-"""
-
-with gr.Blocks(css=css) as space:
+with gr.Blocks() as space:
     gr.Markdown(
         """
-    # Anime Collaborative Filtering Recommender
+    # Anime Collaborative Filtering System
     This is a Pytorch recommendation model that uses neural collaborative filtering.
     Enter an anime, and it will suggest similar shows!
     """
     )
 
-    with gr.Box():
-        gr.Markdown("Enter an anime:")
+    dropdown = gr.Dropdown(label="Enter an anime", choices=animes)
 
-        with gr.Column(elem_id="selection_column"):
-            dropdown = gr.Dropdown(container=False, choices=animes, show_label=False)
-            selection_image = gr.Image(show_label=False, width=225, visible=False)
+    gallery = gr.Gallery(label="Recommendations", rows=2, columns=3)
 
-    gallery = gr.Gallery(label="Recommendations", columns=[2, 2, 3, 3, 4, 5])
-
-    def submit(anime):
-        if anime is None:
-            return {
-                selection_image: gr.update(visible=False),
-                gallery: gr.update(value=[]),
-            }
-
-        selection_image_url = fetch_anime_image(anime)
-        recommendations = recommend(anime)
-
-        return {
-            selection_image: gr.update(visible=True, value=selection_image_url),
-            gallery: gr.update(value=recommendations),
-        }
-
-    dropdown.change(fn=submit, inputs=dropdown, outputs=[selection_image, gallery])
+    dropdown.change(fn=recommend, inputs=dropdown, outputs=gallery)
 
 space.launch()
